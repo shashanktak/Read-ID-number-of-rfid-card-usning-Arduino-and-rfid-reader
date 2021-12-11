e58eab81c6353d59df72fffcd26e9f7e8b9fa9e7
Shashank tak <87747295+shashanktak@users.noreply.github.com>
2021-12-11 18:46:43 +0530

    RFID Card reader

---
+++ rfid_identify.ino
@@ -0,0 +1,69 @@
+
+ /* Pin layout should be as follows:
+ * Signal     Pin              Pin               Pin
+ *            Arduino Uno      Arduino Mega      MFRC522 board
+ * ------------------------------------------------------------
+ * Reset      9                5                 RST
+ * SPI SS     10               53                SDA
+ * SPI MOSI   11               51                MOSI
+ * SPI MISO   12               50                MISO
+ * SPI SCK    13               52                SCK
+ 
+ 
+ * voltage 3.3v  
+ 
+Download Libraries:
+https://www.electroniclinic.com/arduino-libraries-download-and-projects-they-are-used-in-project-codes/
+ */
+ 
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);        // Create MFRC522 instance.
+ 
+void setup() {
+        Serial.begin(9600);        // Initialize serial communications with the PC
+        SPI.begin();                // Init SPI bus
+        mfrc522.PCD_Init();        // Init MFRC522 card
+        //Serial.println("Scan a MIFARE Classic PICC to demonstrate Value Blocks.");
+}
+ 
+void loop() {
+        
+        // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+        MFRC522::MIFARE_Key key;
+        for (byte i = 0; i < 6; i++) {
+                key.keyByte[i] = 0xFF;
+        }
+        // Look for new cards
+        if ( ! mfrc522.PICC_IsNewCardPresent()) {
+                return;
+        }
+ 
+        // Select one of the cards
+        if ( ! mfrc522.PICC_ReadCardSerial()) {
+                return;
+        }
+        // Now a card is selected. The UID and SAK is in mfrc522.uid.
+        
+        // Dump UID
+        Serial.print("Card UID:");
+        for (byte i = 0; i < mfrc522.uid.size; i++) {
+                Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+                Serial.print(mfrc522.uid.uidByte[i], DEC);
+        } 
+        Serial.println();
+ 
+        // Dump PICC type
+        byte piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+        Serial.print("PICC type: ");
+        Serial.println(mfrc522.PICC_GetTypeName(piccType));
+        if (        piccType != MFRC522::PICC_TYPE_MIFARE_MINI 
+                &&        piccType != MFRC522::PICC_TYPE_MIFARE_1K
+                &&        piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+                //Serial.println("This sample only works with MIFARE Classic cards.");
+                return;
+        }
+}
